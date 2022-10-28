e478f19fd57d43c0c7bd2f324f7eef7a19dfacbf
Lewis Barnes <lewis_barnes@live.co.uk>
2022-10-28 17:52:15 +0100

    add fade animation conditionally visible elements

--- src/components/ConditionallyVisible.tsx
+++ src/components/ConditionallyVisible.tsx
@@ -1,12 +1,26 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 export const ConditionallyVisible: React.FC<{
   visibleOn: boolean;
   children?: React.ReactNode;
   className?: string;
-}> = ({ visibleOn, children, className }) => {
+}> = ({ visibleOn, children }) => {
+  const [animation, setAnimation] = useState("");
+
+  useEffect(() => {
+    if (!visibleOn) {
+      setAnimation("animate__animated animate__fadeIn animate__fast");
+    }
+  }, [visibleOn]);
+
   return (
-    <div className={visibleOn ? `visible ${className}` : "invisible"}>
+    <div
+      className={`${
+        visibleOn
+          ? animation
+          : "animate__animated animate__fadeOut animate__fast"
+      } `}
+    >
       {children}
     </div>
   );
